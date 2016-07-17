c09c64d5ac7f7eb95f3f6896a6e8fa5542209242
matt-oconnell <mattoconnell408@gmail.com>
2016-07-17 16:38:53 -0400

    mobile links

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    FastClick.attach(document.body);
+
+    $('.tog').click(function() {
+        var selected = $(this).attr('href'),
+            selectedEl = $('[section-id="' + selected + '"]');
+        if(!selectedEl.hasClass('active')) {
+            $('.active').removeClass('active');
+            selectedEl.addClass('active');
+        }
+        return false;
+    });
+
+    $('.mobile-click').click(function() {
+    	console.log('why?');
+        var href = $(this).attr('href');
+        window.open(href, $(this).data('name'), 'width=320, height=568');
+        return false;
+    });
+
+    resizeHeights();
+    $(window).resize(function() {
+        resizeHeights();
+    });
+
+    function resizeHeights() {
+        var h = $(window).height() - 170;
+        $('.work-wrap, .about-wrap').height(h + 'px');
+    }
+});
