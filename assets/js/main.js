bab12b5e774c642a7192bde831808c951eb06117
Matt O'Connell <mattoconnell408@gmail.com>
2016-11-06 15:13:34 -0500

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
-    FastClick.attach(document.body);
+  FastClick.attach(document.body);
+  const $window = $(window);
+  const emojiMap = {
+    intro: 0x1F3E0,
+    experiments: 0x1F52C,
+    about: 0x1F47E
+  };
+  location.hash = String.fromCodePoint(emojiMap.intro);
 
-    $('.tog').click(function() {
-        var selected = $(this).attr('href'),
-            selectedEl = $('[section-id="' + selected + '"]');
-        if(!selectedEl.hasClass('active')) {
-            $('.active').removeClass('active');
-            selectedEl.addClass('active');
-        }
-        return false;
-    });
+  $('.navigate').click(function() {
+    const selected = $(this).attr('href');
+    const selectedEl = $(`[section-id="${selected}"]`);
+    location.hash = String.fromCodePoint(emojiMap[selected]);
 
-    $('.mobile-click').click(function() {
-        var href = $(this).attr('href');
-        window.open(href, $(this).data('name'), 'width=320, height=568');
-        return false;
-    });
+    $('.active').removeClass('active');
+    selectedEl.addClass('active');
 
-    resizeHeights();
-    $(window).resize(function() {
-        resizeHeights();
-    });
+    return false;
+  });
 
-    function resizeHeights() {
-        var h = $(window).height() - 170;
-        $('.work-wrap, .about-wrap').height(h + 'px');
-    }
+  $('.mobile-click').click(function() {
+    const $this = $(this);
+    const href = $this.attr('href');
+    window.open(href, $this.data('name'), 'width=320, height=568');
+    return false;
+  });
+
+  resizeHeights();
+  $window.resize(resizeHeights);
+
+  function resizeHeights() {
+    const h = $window.height() - 170;
+    $('.work-wrap, .about-wrap').height(`${h}px`);
+  }
 });
